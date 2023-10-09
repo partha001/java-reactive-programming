6c7cf046c1dda6804b8a4b1cf753cec86a572ece
DESKTOP-S8FEQ2U\biswa <biswas.partha1@gmail.com>
2023-10-09 23:42:03 +0530

    the log function in flux

---
+++ 2WorkingWithFlux/src/main/java/com/partha/WorkingWithFlux/Ex6LogFunctionInFlux.java
@@ -0,0 +1,25 @@
+package com.partha.WorkingWithFlux;
+
+import com.github.javafaker.Faker;
+import com.partha.WorkingWithFlux.util.Util;
+
+import reactor.core.publisher.Flux;
+
+public class Ex6LogFunctionInFlux {
+	
+	public static void main(String[] args) {
+
+//		Flux.range(3, 10)
+//		.subscribe(Util.onNext());
+		
+		
+		Flux<String> names = Flux.range(1, 10)
+				.log()
+				.map(item -> Faker.instance().name().fullName());
+		names.subscribe(Util.onNext());
+		
+		
+	}
+
+
+}
