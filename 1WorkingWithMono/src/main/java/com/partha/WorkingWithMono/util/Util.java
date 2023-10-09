c01e7a2576a92fe4885993c5b04c50b168ae3d27
DESKTOP-S8FEQ2U\biswa <biswas.partha1@gmail.com>
2023-10-09 15:52:44 +0530

    adding util

---
+++ 1WorkingWithMono/src/main/java/com/partha/WorkingWithMono/util/Util.java
@@ -0,0 +1,26 @@
+package com.partha.WorkingWithMono.util;
+
+import java.util.function.Consumer;
+
+public class Util {
+	
+	//defining my own custom consumer for onNext() .
+		//note the below method name can be anything
+		public static Consumer<Object> onNext(){
+			return  item -> System.out.println("Received : "+ item);
+		}
+		
+		//my own custom error-consumer
+		public static Consumer<Throwable> onError(){
+			return  e -> System.out.println("Received : "+ e.getMessage());
+		}
+		
+		//my own custom onComplete runnable
+		public static Runnable onComplete() {
+			return () -> System.out.println("Completed");
+		}
+		
+
+
+
+}
